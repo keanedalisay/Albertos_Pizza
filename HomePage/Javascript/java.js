1cd417574f76e997fec6b58ce13be37bb179a0d3
keanedalisay <keandals21@gmail.com>
2024-04-13 20:38:50 +0800

    Refactor header and footer functionality and appearace

--- HomePage/Javascript/java.js
+++ HomePage/Javascript/java.js
@@ -1,4 +1,10 @@
-function toggleMenu() {
-    var menuList = document.getElementById("menu-list");
-    menuList.classList.toggle("show"); // Toggle the 'show' class on the menu list
-}
+(function (){
+    let menuBtn = document.querySelector("[data-js=menu_btn]")
+    menuBtn.addEventListener("click", function () {
+        let menuMobile = document.querySelector("[data-js=menu_mobile]");
+        let menuMobileIcon = document.querySelector("[data-js=menu_btn_icon]");
+        menuBtn.classList.toggle("clicked");
+        menuMobile.classList.toggle("show"); 
+        menuMobileIcon.classList.toggle("rotate");
+    })
+})();
