8103dae63d99e9f1538d52452471cc374b5019c4
keanedalisay <keandals21@gmail.com>
2024-04-13 21:38:39 +0800

    Reorganize file system to have the public and src directory
    Remove HomePage directory and transfer contents to public and src directory

---
+++ public/scripts/global.js
@@ -0,0 +1,10 @@
+(function (){
+  let menuBtn = document.querySelector("[data-js=menu_btn]")
+  menuBtn.addEventListener("click", function () {
+      let menuMobile = document.querySelector("[data-js=menu_mobile]");
+      let menuMobileIcon = document.querySelector("[data-js=menu_btn_icon]");
+      menuBtn.classList.toggle("clicked");
+      menuMobile.classList.toggle("show"); 
+      menuMobileIcon.classList.toggle("rotate");
+  })
+})();
